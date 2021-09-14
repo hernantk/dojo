ff88818f1cd1866b2a2317e725e784579687c0a4
hernan <Hernantkup@gmail.com>
2021-09-13 22:12:26 -0300

    ok

---
+++ Test.py
@@ -0,0 +1,22 @@
+from _typeshed import Self
+import unittest
+
+class TestRomano(unittest.TestCase):
+
+
+
+    def test_number_repeat_max(self,number):
+        
+        
+        self.assertEqual(333,"CCCXXXIII")
+        self.assertEqual("CCCXXXIII",333)
+
+    def test_number_more_of_(self,number):
+
+        self.assertEqual(62,"LXII")
+        self.assertEqual("LXII",62)
+    def test_number_repeat_max(self,number):
+
+        self.assertEqual(90,"XC")
+        self.assertEqual("XC",90)
+        
